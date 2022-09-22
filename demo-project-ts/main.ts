ffe52887e77223377a3df57d75938d8305f87f86
Vani Jain <vani.jain_cs19@gla.ac.in>
2022-09-22 11:54:30 +0530

    files

---
+++ demo-project-ts/main.ts
@@ -0,0 +1,11 @@
+namespace ingenuity.game{
+    export class Main{
+        constructor(){
+            this.showMessage();
+        }
+
+        private showMessage():void{
+            console.log("Welcome");
+        }
+    }
+}
